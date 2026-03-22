700bc4670d8f99fcea8f496c88e9ad5c248da329
gioLomb <nicolalarora71@gmail.com>
2026-03-22 13:14:38 +0100

    bitwise hex_to_int implemented

--- route_handler.c
+++ route_handler.c
@@ -148,11 +148,9 @@ int handler_delete(Hash_Table* table, const char* url, char* responseBuffer){
     }  
 }
 
-static int hex_to_int(char c) {
-    if (c >= '0' && c <= '9') return c - '0';
-    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
-    return -1;
+static inline int hex_to_int(char c) {
+    int is_digit = (c >> 6) ^ 1;
+    return (c & 0xF) + (is_digit ^ 1) * 9;
 }
 
 static int hex_to_int(char c) {
